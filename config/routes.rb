d0972936acf83be82735b09cfa6996d8d3d51592
dlanoronald123 <dlanoronald123@gmail.com>
2021-06-01 19:43:31 +0800

    add styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # root to: 'home#index'
+  root to: 'home#index'
   
   resources :categories do 
     resources :tasks
   end
 end
+
+
